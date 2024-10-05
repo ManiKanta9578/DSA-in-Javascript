026addf1f7cfcfd7fde401dfa6c344cbb53bf2f3
ManiKanta <112609309+ManiKanta9578@users.noreply.github.com>
2024-10-05 17:49:09 +0000

    sorts an array in descending order

--- 22_sort_an_array_of_numbers.js
+++ 22_sort_an_array_of_numbers.js
@@ -1,11 +1,13 @@
-const bubbleSort = (arr) => {
+//Ascending order
+
+const bubbleSortAsc = (arr) => {
 
     for (let i = 0; i < arr.length - 1; i++) {
         for (let j = 0; j < arr.length - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 let temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
@@ -13,4 +15,23 @@ const bubbleSort = (arr) => {
 }
 
 const arr = [64, 34, 25, 12, 22, 11, 90];
-console.log(bubbleSort(arr));
+console.log(bubbleSortAsc(arr));
+
+//Descending order
+
+const bubbleSortDes = (arr) => {
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - 1; j++) {
+            if (arr[j] < arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+const arr2 = [64, 34, 25, 12, 22, 11, 90];
+console.log(bubbleSortDes(arr2));
