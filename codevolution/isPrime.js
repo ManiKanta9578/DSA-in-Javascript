9a9db4c4e3bcdc772f030d76d904e08aaf4155f1
ManiKanta <112609309+ManiKanta9578@users.noreply.github.com>
2024-12-15 15:30:00 +0000

    factorial and isPrime

---
+++ codevolution/isPrime.js
@@ -0,0 +1,29 @@
+const isPrime = (n) => {
+    if(n < 2) return false;
+
+    for(let i = 2; i < n; i++){
+        if(n % i === 0){
+            return false;
+        }
+    }
+    return true;
+};
+
+//Big-O = O(n)
+
+//optimised solution
+
+const isPrime_Optimised = (n) => {
+    if(n < 2) return false;
+
+    for(let i = 2; i < Math.sqrt(n); i++){
+        if(n % i === 0){
+            return false;
+        }
+    }
+    return true;
+};
+
+console.log(isPrime(1));
+console.log(isPrime(55));
+console.log(isPrime(7));
