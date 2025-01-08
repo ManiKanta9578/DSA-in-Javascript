228bf52c156f4f04fe6a1f55c376d322e13afdd3
ManiKanta <112609309+ManiKanta9578@users.noreply.github.com>
2025-01-08 02:24:29 +0000

    Javascript Data Structure || LinkedList overview and class

---
+++ codevolution/JavaScript Data Structures/14-LinkedListClass.js
@@ -0,0 +1,26 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    isEmpty() {
+        return this.size === 0;
+    }
+
+    getSize() {
+        return this.size;
+    }
+}
+
+let list = new LinkedList();
+
+console.log("List is Empty? ",list.isEmpty());
+console.log("List Size: ",list.getSize());
